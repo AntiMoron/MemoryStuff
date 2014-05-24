c6cb7a1ac7f7ba581c0915de7410f96b25660f92
AntiMoron <anti2moron@gmail.com>
2014-05-24 23:09:32 +0800

    v 0.0.1
    Get Started

---
+++ BasicStuff.hpp
@@ -0,0 +1,46 @@
+/***************************
+//@ScopeGuard
+//Author : AntiMoron
+//Date   : 2014-05-24
+***************************/
+
+#ifndef __BASICSTUFF_h__
+#define __BASICSTUFF_h__
+
+#include<type_traits>
+#include<memory>
+#include<functional>
+#include<iostream>
+
+class ScopeGuard
+{
+public:
+	~ScopeGuard()
+	{
+		if(foo == nullptr)
+			return ;
+		foo();
+		return ;
+	}
+private:
+	friend ScopeGuard make_guard(std::function<void()> );
+	explicit ScopeGuard()
+	{
+		foo = nullptr;
+	}
+	const ScopeGuard& operator = (const ScopeGuard& other)
+	{
+		foo = other.foo;
+	}
+	std::function<void()> foo;
+};
+
+ScopeGuard make_guard(std::function<void()> foo_ptr)
+{
+	ScopeGuard result;
+	result.foo = foo_ptr;
+	return result;
+}
+
+#endif
+
---
+++ Source.cpp
@@ -0,0 +1,36 @@
+/***************************
+//@love letter
+//Author : AntiMoron
+//Date   : 2014-05-24
+***************************/
+
+#include"BasicStuff.hpp"
+#include<iostream>
+
+struct you
+{
+	you(bool _val = false) : LoveMe(_val)
+	{
+		troubles = nullptr;
+	}
+	bool LoveMe;
+	char* troubles;
+};
+
+int main()
+{
+	you U;
+	auto I = make_guard(
+						[&]()
+						{
+							std::cout<<"I'll be your guard whether you love me or not."<<std::endl;
+							if(U.troubles)
+							{
+								delete[] U.troubles;
+								U.troubles = nullptr;
+							}
+						});
+	U.troubles = new char[256];
+	std::cout<<"Guard your memory.Keep you safe."<<std::endl;
+	return 0;
+}
